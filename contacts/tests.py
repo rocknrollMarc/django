afd2391dfbc5744f5a6e55f09fda68d3d8bb827e
Marc Bluemner <marc.bluemner@gmail.com>
2015-10-10 16:21:55 +0200

    Contact test case

--- contacts/tests.py
+++ contacts/tests.py
@@ -8,9 +8,14 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ContactTests(TestCase):
+    """Contact model tests."""
+
+    def test_str(self):
+
+        contact = Contact(first_name='John', last_name='Smith')
+
+        self.assertEquals(
+            str(contact),
+            'John Smith',
+        )
